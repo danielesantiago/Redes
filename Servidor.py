02beff996173c806632ff97505502c496fa8e41d
Daniele Santiago <daniellestg6@gmail.com>
2023-01-16 19:16:55 -0300

    permitir n conexoes

--- Servidor.py
+++ Servidor.py
@@ -4,13 +4,13 @@ Nomes: Ana Beatriz Juvencio - 801817, Daniele Pereira Santiago - 792175
 
 '''
 
-
 import os
 import socket
 import threading
 
-def handle_client(client_socket):
 
+def handle_client(client_socket):
+    # Diretório onde os arquivos serão salvos
     path = "C:\\Users\\danie\\OneDrive\\Documentos\\Redes\\"
 
     while True:
@@ -25,9 +25,9 @@ def handle_client(client_socket):
         print("Recebeu requisição para %s arquivo %s" % (operation, nome_arquivo))
         print(file_name)
 
-        
         if operation == "enviar":
-            file_path = os.path.join(path,'RecebidoServidor\\' + nome_arquivo)
+            # Caminho para salvar o arquivo
+            file_path = os.path.join(path, 'RecebidoServidor\\' + nome_arquivo)
             # Recebe o conteúdo do arquivo em bytes
             conteudo = client_socket.recv(1024).decode()
             # Salva em disco
@@ -36,7 +36,7 @@ def handle_client(client_socket):
             f.close()
             print("Arquivo recebido com sucesso")
             client_socket.send("Arquivo recebido com sucesso\n".encode())
-            
+
 
 
         elif operation == "receber":
@@ -44,11 +44,11 @@ def handle_client(client_socket):
                 # Lê o arquivo do disco e envia os bytes oa cliente
                 with open(os.path.join(path, 'RecebidoServidor\\' + nome_arquivo), "rb") as f:
                     data = f.read()
-                    client_socket.send(data +"\n".encode())
+                    client_socket.send(data + "\n".encode())
                 f.close()
 
                 client_socket.send("Arquivo enviado com sucesso\n".encode())
-                
+
             except IOError:
                 # Envia uma mensagem de erro para o cliente caso o arquivo não exista no servidor
                 client_socket.send("Arquivo não encontrado no servidor\n".encode())
@@ -59,10 +59,13 @@ def handle_client(client_socket):
 def main():
     # Inicia o socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Liga o socket ao endereço IP e porta especificados
     server_socket.bind(("0.0.0.0", 1238))
-    server_socket.listen(5)
+    # Escuta por conexões entrantes
+    server_socket.listen(0) # Permite quantas conexões o sistema operacional aguentar
 
     while True:
+        # Aceita a conexão de um cliente
         client_socket, client_address = server_socket.accept()
         print(f"Conexão de {client_address}")
 
